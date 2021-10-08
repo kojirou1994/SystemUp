7dd317a1c918652a5836267e882be649e78c7238
kojirou <kojirouhtc@gmail.com>
2021-10-08 13:22:25 +0800

    add cwd test

--- Tests/SystemExtensionTests/SystemExtensionTests.swift
+++ Tests/SystemExtensionTests/SystemExtensionTests.swift
@@ -4,5 +4,13 @@ import SystemUp
 import Foundation
 
 final class SystemExtensionTests: XCTestCase {
+  func testCWD() {
+    let old = FileUtility.currentDirectoryPath
 
+    XCTAssertNoThrow(try FileUtility.changeCurrentDirectoryPath(".."))
+
+    let new = FileUtility.currentDirectoryPath
+
+    XCTAssertEqual(old.removingLastComponent(), new)
+  }
 }
