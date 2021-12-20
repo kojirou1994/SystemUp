96fd4b74793709d3292f1249cfb2041daf64067d
kojirou <kojirouhtc@gmail.com>
2021-12-21 03:30:36 +0800

    add fts

---
+++ Sources/SystemUp/Fts.swift
@@ -0,0 +1,354 @@
+import SystemPackage
+import CSystemUp
+#if canImport(Darwin)
+import Darwin
+#elseif canImport(Glibc)
+import Glibc
+#endif
+
+public struct Fts {
+  private init(_ handle: UnsafeMutablePointer<FTS>) {
+    self.handle = handle
+  }
+
+  private let handle: UnsafeMutablePointer<FTS>
+
+  public static func open(path: FilePath, options: Options = []) throws -> Self {
+    assert(!path.isEmpty)
+
+    return try path.withPlatformString { path in
+      var array: (UnsafeMutablePointer<Int8>?, UnsafeMutablePointer<Int8>?) = (UnsafeMutablePointer(mutating: path), nil)
+      return try withUnsafeMutablePointer(to: &array) { pointer in
+        try _fts_open(UnsafeMutableRawPointer(pointer).assumingMemoryBound(to: UnsafeMutablePointer<Int8>?.self), options)
+      }
+    }
+  }
+
+  public static func open<C: Collection>(paths: C, options: Options = []) throws -> Self where C.Element == FilePath {
+
+    let arraySize = paths.count + 1
+    let pathArray = UnsafeMutablePointer<UnsafeMutablePointer<Int8>?>.allocate(capacity: arraySize)
+    defer {
+      pathArray.deinitialize(count: arraySize)
+      pathArray.deallocate()
+    }
+    paths.enumerated().forEach { offset, path in
+      assert(!path.isEmpty)
+      // TODO: unsafe, but safe
+      path.withPlatformString { path in
+        pathArray.advanced(by: offset).initialize(to: UnsafeMutablePointer(mutating: path))
+      }
+    }
+    pathArray.advanced(by: paths.count).initialize(to: nil)
+
+    return try _fts_open(pathArray, options)
+  }
+
+  internal static func _fts_open(_ array: UnsafePointer<UnsafeMutablePointer<CChar>?>, _ options: Options) throws -> Self {
+    precondition(options.contains(.logical) || options.contains(.physical), "at least one of which (either FTS_LOGICAL or FTS_PHYSICAL) must be specified")
+
+    guard let ptr = fts_open(array, options.rawValue, nil) else {
+      throw Errno.current
+    }
+    return .init(ptr)
+  }
+
+  private func optionalOrErrno<T>(_ v: T?) throws -> T? {
+    let errno = Errno.current
+    if v == nil, errno.rawValue != 0 {
+      throw errno
+    }
+    return v
+  }
+
+  public func read() throws -> UnsafeMutablePointer<Fts.Entry>? {
+    let ptr = try optionalOrErrno(fts_read(handle))
+    return .init(OpaquePointer(ptr))
+  }
+
+  public func children(options: ChildrenOptions = []) throws -> UnsafeMutablePointer<Fts.Entry>? {
+    let ptr = try optionalOrErrno(fts_children(handle, options.rawValue))
+    return .init(OpaquePointer(ptr))
+  }
+
+  public func set(entry: UnsafeMutablePointer<Fts.Entry>, option: SetOptions) throws {
+    try nothingOrErrno(retryOnInterrupt: false) {
+      fts_set(handle, &entry.pointee.entry, option.rawValue)
+    }.get()
+  }
+
+  public func close() throws {
+    try nothingOrErrno(retryOnInterrupt: false) {
+      fts_close(handle)
+    }.get()
+  }
+
+  public func closeAfter<R>(_ body: (Self) throws -> R) throws -> R {
+    let r: R
+    do {
+      r = try body(self)
+    } catch {
+      try? close()
+      throw error
+    }
+    try close()
+    return r
+  }
+}
+
+extension Fts {
+
+  public struct Options: OptionSet {
+
+    public init(rawValue: Int32) {
+      self.rawValue = rawValue
+    }
+
+    init(_ rawValue: Int32) {
+      self.rawValue = .init(rawValue)
+    }
+
+    public let rawValue: Int32
+
+    /// This option causes any symbolic link specified as a root path to be followed immediately whether or not
+    /// FTS_LOGICAL is also specified.
+    public static var comFollow: Self { .init(FTS_COMFOLLOW) }
+
+    /// This option causes the fts routines to return FTSENT structures for the targets of symbolic links instead of
+    /// the symbolic links themselves.  If this option is set, the only symbolic links for which FTSENT structures
+    /// are returned to the application are those referencing non-existent files.  Either FTS_LOGICAL or FTS_PHYSICAL
+    /// must be provided to the fts_open() function.
+    public static var logical: Self { .init(FTS_LOGICAL) }
+
+    /// As a performance optimization, the fts functions change directories as they walk the file hierarchy.  This
+    /// has the side-effect that an application cannot rely on being in any particular directory during the traver-
+    /// sal.  The FTS_NOCHDIR option turns off this optimization, and the fts functions will not change the current
+    /// directory.  Note that applications should not themselves change their current directory and try to access
+    /// files unless FTS_NOCHDIR is specified and absolute pathnames were provided as arguments to fts_open().
+    public static var noChdir: Self { .init(FTS_NOCHDIR) }
+
+    /// By default, returned FTSENT structures reference file characteristic information (the statp field) for each
+    /// file visited.  This option relaxes that requirement as a performance optimization, not calling stat(2) when-
+    /// ever possible.  If stat(2) doesn't need to be called, the fts functions will set the fts_info field to
+    /// FTS_NSOK; otherwise fts_info will be set to the correct file information value corresponding to the stat(2)
+    /// information.  In any case, the statp field will always be undefined.  Note that because fts detects directory
+    /// cycles and dangling symbolic links, stat(2) is always called for directories and is called for symbolic links
+    /// when FTS_LOGICAL is set.
+    public static var noStat: Self { .init(FTS_NOSTAT) }
+
+    /// Like FTS_NOSTAT but if the file type is returned by readdir(3), the corresponding file information value is
+    /// returned in fts_info instead of FTS_NSOK.
+    public static var noStatType: Self { .init(FTS_NOSTAT_TYPE) }
+
+    /// This option causes the fts routines to return FTSENT structures for symbolic links themselves instead of the
+    /// target files they point to.  If this option is set, FTSENT structures for all symbolic links in the hierarchy
+    /// are returned to the application.  Either FTS_LOGICAL or FTS_PHYSICAL must be provided to the fts_open() function.
+    public static var physical: Self { .init(FTS_PHYSICAL) }
+
+    /// By default, unless they are specified as path arguments to fts_open(), any files named `.' or `..' encoun-
+    /// tered in the file hierarchy are ignored.  This option causes the fts routines to return FTSENT structures for them.
+    public static var seedot: Self { .init(FTS_SEEDOT) }
+
+    /// This option prevents fts from descending into directories that have a different device number than the file
+    /// from which the descent began.
+    public static var xDev: Self { .init(FTS_XDEV) }
+
+  }
+
+}
+
+extension Fts {
+
+  public struct Entry {
+
+    @_alwaysEmitIntoClient
+    fileprivate var entry: FTSENT
+
+    @_alwaysEmitIntoClient
+    public var info: UInt16 {
+      entry.fts_info
+    }
+
+    @_alwaysEmitIntoClient
+    public var pathToCurrentDirectory: FilePath {
+      .init(platformString: entry.fts_accpath)
+    }
+
+    @_alwaysEmitIntoClient
+    public var path: FilePath {
+      .init(platformString: entry.fts_path)
+    }
+
+    @_alwaysEmitIntoClient
+    public var name: Int8 {
+      entry.fts_name
+    }
+
+    @_alwaysEmitIntoClient
+    public var namelen: UInt16 {
+      entry.fts_namelen
+    }
+
+    @_alwaysEmitIntoClient
+    public var level: Int16 {
+      entry.fts_level
+    }
+
+    @_alwaysEmitIntoClient
+    public var errno: Errno? {
+      entry.fts_errno == 0 ? nil : .init(rawValue: entry.fts_errno)
+    }
+
+    @_alwaysEmitIntoClient
+    public var number: Int {
+      get {
+        entry.fts_number
+      }
+      _modify {
+        yield &entry.fts_number
+      }
+    }
+//
+//    @_alwaysEmitIntoClient
+//    public var pointer: UnsafeMutableRawPointer {
+//      entry.fts_pointer
+//    }
+
+    @_alwaysEmitIntoClient
+    public var parent: UnsafeMutablePointer<Self> {
+      .init(OpaquePointer(entry.fts_parent))
+    }
+
+    @_alwaysEmitIntoClient
+    public var link: UnsafeMutablePointer<Self>? {
+      .init(OpaquePointer(entry.fts_link))
+    }
+
+    @_alwaysEmitIntoClient
+    public var cycle: UnsafeMutablePointer<Self> {
+      .init(OpaquePointer(entry.fts_cycle))
+    }
+
+    @_alwaysEmitIntoClient
+    public var fileStatus: UnsafeMutablePointer<FileStatus>? {
+      .init(OpaquePointer(entry.fts_statp))
+    }
+
+    var status: FileStatus {
+      get {
+        FileStatus(status: entry.fts_statp.pointee)
+      }
+      _modify {
+        yield &UnsafeMutablePointer<FileStatus>(OpaquePointer(entry.fts_statp))!.pointee
+      }
+    }
+
+    @_alwaysEmitIntoClient
+    func aa() {
+
+    }
+  }
+
+  public struct ChildrenOptions: OptionSet {
+
+    public init(rawValue: Int32) {
+      self.rawValue = rawValue
+    }
+
+    init(_ rawValue: Int32) {
+      self.rawValue = .init(rawValue)
+    }
+
+    public let rawValue: Int32
+
+    /// Only the names of the files are needed.  The contents of all the fields in the returned linked list of struc-
+    /// tures are undefined with the exception of the fts_name and fts_namelen fields.
+    public static var nameOnly: Self { .init(FTS_NAMEONLY) }
+  }
+
+  public struct SetOptions: RawRepresentable {
+
+    public init(rawValue: Int32) {
+      self.rawValue = rawValue
+    }
+
+    init(_ rawValue: Int32) {
+      self.rawValue = .init(rawValue)
+    }
+
+    public let rawValue: Int32
+
+    /// Re-visit the file; any file type may be re-visited.  The next call to fts_read() will return the referenced
+    /// file.  The fts_stat and fts_info fields of the structure will be reinitialized at that time, but no other fields
+    /// will have been changed.  This option is meaningful only for the most recently returned file from fts_read().
+    /// Normal use is for post-order directory visits, where it causes the directory to be re-visited (in both pre and
+    /// post-order) as well as all of its descendants.
+    public static var again: Self { .init(FTS_AGAIN) }
+
+    /// The referenced file must be a symbolic link.  If the referenced file is the one most recently returned by
+    /// fts_read(), the next call to fts_read() returns the file with the fts_info and fts_statp fields reinitialized to
+    /// reflect the target of the symbolic link instead of the symbolic link itself.  If the file is one of those most
+    /// recently returned by fts_children(), the fts_info and fts_statp fields of the structure, when returned by
+    /// fts_read(), will reflect the target of the symbolic link instead of the symbolic link itself.  In either case,
+    /// if the target of the symbolic link does not exist the fields of the returned structure will be unchanged and the
+    /// fts_info field will be set to FTS_SLNONE.
+    /// If the target of the link is a directory, the pre-order return, followed by the return of all of its descen-
+    /// dants, followed by a post-order return, is done.
+    public static var follow: Self { .init(FTS_FOLLOW) }
+
+    /// No descendants of this file are visited.  The file may be one of those most recently returned by either
+    /// fts_children() or fts_read().
+    public static var skip: Self { .init(FTS_SKIP) }
+  }
+
+  public struct Info: RawRepresentable {
+
+    public init(rawValue: UInt16) {
+      self.rawValue = rawValue
+    }
+
+    init(_ rawValue: Int32) {
+      self.rawValue = .init(rawValue)
+    }
+
+    public let rawValue: UInt16
+
+    /// A directory being visited in pre-order.
+    public static var directory: Self { .init(FTS_D) }
+
+    /// A directory that causes a cycle in the tree.  (The fts_cycle field of the FTSENT structure will be filled in as well.)
+    public static var directoryCycle: Self { .init(FTS_DC) }
+
+    /// Any FTSENT structure that represents a file type not explicitly described by one of the other fts_info values.
+    public static var `default`: Self { .init(FTS_DEFAULT) }
+
+    /// A directory which cannot be read.  This is an error return, and the fts_errno field will be set to indicate what caused the error.
+    public static var directoryNotRead: Self { .init(FTS_DNR) }
+
+    /// A file named ‘.’ or ‘..’ which was not specified as a file name to fts_open() or fts_open_b() (see FTS_SEEDOT).
+    public static var dot: Self { .init(FTS_DOT) }
+
+    /// A directory being visited in post-order.  The contents of the FTSENT structure will be unchanged from when it was returned in pre-order, i.e. with the fts_info field set to FTS_D.
+    public static var directoryPost: Self { .init(FTS_DP) }
+
+    /// This is an error return, and the fts_errno field will be set to indicate what caused the error.
+    public static var error: Self { .init(FTS_ERR) }
+
+    /// A regular file.
+    public static var file: Self { .init(FTS_F) }
+
+    /// A file for which no stat(2) information was available.  The contents of the fts_statp field are undefined.  This is an error return, and the fts_errno field will be set to indicate what caused the error.
+    public static var fileNoStat: Self { .init(FTS_NS) }
+
+    /// A file for which no stat(2) information was requested.  The contents of the fts_statp field are undefined.
+    public static var directoryNoStat: Self { .init(FTS_NSOK) }
+
+    /// A symbolic link.
+    public static var symbolic: Self { .init(FTS_SL) }
+
+    /// A symbolic link with a non-existent target.  The contents of the fts_statp field reference the file characteristic information for the symbolic link itself.
+    public static var symbolicNonExistent: Self { .init(FTS_SLNONE) }
+
+  }
+
+}
