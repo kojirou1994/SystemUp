6d66bc66f992e45f69d7944c1bfb54dc1287750d
kojirou <kojirouhtc@gmail.com>
2022-04-07 04:31:36 +0800

    rewrite CloneFile

--- Sources/SystemUp/CloneFile.swift
+++ Sources/SystemUp/CloneFile.swift
@@ -1,35 +1,49 @@
-import SystemPackage
 #if canImport(Darwin)
 import Darwin
-#elseif canImport(Glibc)
-import Glibc
-#endif
+import SystemPackage
 
-public struct CloneFlags: OptionSet {
-  public init(rawValue: UInt32) {
-    self.rawValue = rawValue
-  }
-  init(_ rawValue: Int32) {
-    self.rawValue = .init(rawValue)
-  }
-  public let rawValue: UInt32
+@available(macOS 10.12, iOS 10.0, tvOS 10.0, watchOS 3.0, *)
+public extension FileUtility {
 
-  public static var noFollow: Self { .init(CLONE_NOFOLLOW) }
-  public static var noOwnerCopy: Self { .init(CLONE_NOOWNERCOPY) }
-
-}
-
-extension FileUtility {
-
-  @available(macOS 10.12, *)
-  public static func cloneFile(from src: FilePath, to dst: FilePath, flags: CloneFlags = []) throws {
+  static func cloneFile(from src: FilePathOption, to dst: FilePathOption, flags: CloneFlags = []) throws {
     try nothingOrErrno(retryOnInterrupt: false) {
-      src.withPlatformString { src in
-        dst.withPlatformString { dst in
-          clonefile(src, dst, flags.rawValue)
+      src.path.withPlatformString { srcPath in
+        dst.path.withPlatformString { dstPath in
+          clonefileat(
+            src.relativedDirFD.rawValue, srcPath,
+            dst.relativedDirFD.rawValue, dstPath,
+            flags.rawValue
+          )
         }
       }
     }.get()
   }
+
+  static func cloneFile(from fd: FileDescriptor, to dst: FilePathOption, flags: CloneFlags = []) throws {
+    try nothingOrErrno(retryOnInterrupt: false) {
+      dst.path.withPlatformString { dstPath in
+        fclonefileat(
+          fd.rawValue,
+          dst.relativedDirFD.rawValue, dstPath,
+          flags.rawValue
+        )
+      }
+    }.get()
+  }
+
+  struct CloneFlags: OptionSet {
+    public init(rawValue: UInt32) {
+      self.rawValue = rawValue
+    }
+    public let rawValue: UInt32
+
+    @_alwaysEmitIntoClient
+    public static var noFollow: Self { .init(rawValue: numericCast(CLONE_NOFOLLOW)) }
+    @_alwaysEmitIntoClient
+    public static var noOwnerCopy: Self { .init(rawValue: numericCast(CLONE_NOOWNERCOPY)) }
+
+  }
   
 }
+
+#endif // Darwin platform
