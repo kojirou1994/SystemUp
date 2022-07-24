8e8521b63b28787753c8cb36f852f4e28d24b9e2
kojirou <kojirouhtc@gmail.com>
2022-07-24 14:57:18 +0800

    CloneFile: add documents

--- Sources/SystemUp/CloneFile.swift
+++ Sources/SystemUp/CloneFile.swift
@@ -5,6 +5,7 @@ import SystemPackage
 @available(macOS 10.12, iOS 10.0, tvOS 10.0, watchOS 3.0, *)
 public extension FileSyscalls {
 
+  /// create copy on write clones of files
   static func cloneFile(from src: FilePathOption, to dst: FilePathOption, flags: CloneFlags = []) -> Result<Void, Errno> {
     nothingOrErrno(retryOnInterrupt: false) {
       src.path.withPlatformString { srcPath in
@@ -19,6 +20,7 @@ public extension FileSyscalls {
     }
   }
 
+  /// create copy on write clones of files
   static func cloneFile(from fd: FileDescriptor, to dst: FilePathOption, flags: CloneFlags = []) -> Result<Void, Errno> {
     nothingOrErrno(retryOnInterrupt: false) {
       dst.path.withPlatformString { dstPath in
@@ -37,8 +39,11 @@ public extension FileSyscalls {
     }
     public let rawValue: UInt32
 
+    /// Don't follow the src file if it is a symbolic link (applicable only if the source is not a directory).  The symbolic link is itself cloned if src names a symbolic link.
     @_alwaysEmitIntoClient
     public static var noFollow: Self { .init(macroValue: CLONE_NOFOLLOW) }
+
+    /// Don't copy ownership information from the source when run called with superuser privileges.  The symbolic link is itself cloned if src names a symbolic link.
     @_alwaysEmitIntoClient
     public static var noOwnerCopy: Self { .init(macroValue: CLONE_NOOWNERCOPY) }
 
