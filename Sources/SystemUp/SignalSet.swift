0b47e63b23caaf2f11a09023c2be6ecae79b39f8
kojirou <kojirouhtc@gmail.com>
2022-08-02 14:01:28 +0800

    add SignalSet

---
+++ Sources/SystemUp/SignalSet.swift
@@ -0,0 +1,37 @@
+#if canImport(Darwin)
+import Darwin
+#elseif canImport(Glibc)
+import Glibc
+#endif
+
+public struct SignalSet: RawRepresentable {
+  public var rawValue: sigset_t
+
+  public init(rawValue: sigset_t) {
+    self.rawValue = rawValue
+  }
+}
+
+public extension SignalSet {
+  mutating func add(signal: CInt) {
+    sigaddset(&rawValue, signal)
+  }
+
+  mutating func delete(signal: CInt) {
+    sigdelset(&rawValue, signal)
+  }
+
+  mutating func removeAll() {
+    sigemptyset(&rawValue)
+  }
+
+  mutating func fill() {
+    sigfillset(&rawValue)
+  }
+
+  func contains(signal: CInt) -> Bool {
+    withUnsafePointer(to: rawValue) { sigset in
+      sigismember(sigset, signal) == 1
+    }
+  }
+}
