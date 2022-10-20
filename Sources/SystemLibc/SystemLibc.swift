afe1714f771f259eea0d1eef8ef99a709823cdc0
kojirou <kojirouhtc@gmail.com>
2022-10-20 12:10:57 +0800

    updates

--- Sources/SystemLibc/SystemLibc.swift
+++ Sources/SystemLibc/SystemLibc.swift
@@ -3,3 +3,9 @@
 #elseif canImport(Glibc)
 @_exported import Glibc
 #endif
+
+
+#if os(Linux)
+@_silgen_name("vasprintf")
+public func vasprintf(_ ret: UnsafeMutablePointer<UnsafeMutablePointer<CChar>?>!, _ format: UnsafePointer<CChar>!, _ va: CVaListPointer) -> Int32
+#endif
--- Sources/SystemUp/FormattedString.swift
+++ Sources/SystemUp/FormattedString.swift
@@ -1,19 +1,32 @@
 import CUtility
 import SystemLibc
-
-#if os(Linux)
-@_silgen_name("vasprintf")
-private func vasprintf(_ ret: UnsafeMutablePointer<UnsafeMutablePointer<CChar>?>!, _ format: UnsafePointer<CChar>!, _ va: CVaListPointer) -> Int32
-#endif
+import SystemPackage
 
 public extension LazyCopiedCString {
   convenience init(format: UnsafePointer<CChar>, _ args: CVarArg...) throws {
     var size: Int32 = 0
     let cString = try safeInitialize { str in
       withVaList(args) { va in
-        size = vasprintf(&str, format, va)
+        size = SystemLibc.vasprintf(&str, format, va)
       }
     }
     self.init(cString: cString, forceLength: Int(size), freeWhenDone: true)
+  }
+}
+
+public extension FileStream {
+  func write(format: UnsafePointer<CChar>, _ args: CVarArg...) -> Int32 {
+    withVaList(args) { va in
+      SystemLibc.vfprintf(rawValue, format, va)
+    }
+  }
+}
+
+
+public extension FileDescriptor {
+  func write(format: UnsafePointer<CChar>, _ args: CVarArg...) -> Int32 {
+    withVaList(args) { va in
+      SystemLibc.vdprintf(rawValue, format, va)
+    }
   }
 }
