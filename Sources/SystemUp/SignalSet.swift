d1680c06d81f84c7dd299ba24784831cc64d75b6
kojirou <kojirouhtc@gmail.com>
2022-10-27 07:56:32 +0800

    update SignalSet

--- Sources/SystemUp/SignalSet.swift
+++ Sources/SystemUp/SignalSet.swift
@@ -1,39 +1,49 @@
-#if canImport(Darwin)
-import Darwin
-#elseif canImport(Glibc)
-import Glibc
-#endif
+import SystemLibc
 
 public struct SignalSet: RawRepresentable {
   public var rawValue: sigset_t
 
+  @inlinable @inline(__always)
+  @_alwaysEmitIntoClient
   public init(rawValue: sigset_t) {
     self.rawValue = rawValue
   }
 
+  @inlinable @inline(__always)
+  @_alwaysEmitIntoClient
   public init() {
     self.rawValue = .init()
   }
 }
 
 public extension SignalSet {
-  mutating func add(signal: CInt) {
+  @inlinable @inline(__always)
+  @_alwaysEmitIntoClient
+  mutating func add(signal: Int32) {
     sigaddset(&rawValue, signal)
   }
 
-  mutating func delete(signal: CInt) {
+  @inlinable @inline(__always)
+  @_alwaysEmitIntoClient
+  mutating func delete(signal: Int32) {
     sigdelset(&rawValue, signal)
   }
 
+  @inlinable @inline(__always)
+  @_alwaysEmitIntoClient
   mutating func removeAll() {
     sigemptyset(&rawValue)
   }
 
+  @inlinable @inline(__always)
+  @_alwaysEmitIntoClient
   mutating func fillAll() {
     sigfillset(&rawValue)
   }
 
-  func contains(signal: CInt) -> Bool {
+  @inlinable @inline(__always)
+  @_alwaysEmitIntoClient
+  func contains(signal: Int32) -> Bool {
     withUnsafePointer(to: rawValue) { sigset in
       sigismember(sigset, signal) == 1
     }
