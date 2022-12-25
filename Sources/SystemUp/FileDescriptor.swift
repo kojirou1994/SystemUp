67fe5256b9b85c00c561492f5d721975b7daab23
kojirou <kojirouhtc@gmail.com>
2022-12-26 05:38:18 +0800

    FileDescriptor: add tty functions

--- Sources/SystemUp/FileDescriptor.swift
+++ Sources/SystemUp/FileDescriptor.swift
@@ -1,6 +1,7 @@
 import SystemPackage
 import SystemLibc
 import SyscallValue
+import CUtility
 
 extension FileDescriptor {
   public func read<T: SyscallValue>(upToCount count: Int) throws -> T {
@@ -10,5 +11,56 @@ extension FileDescriptor {
   }
 
   @inlinable @inline(__always)
-  public static var currentWorkingDirectory: Self { .init(rawValue: SystemLibc.AT_FDCWD) }
+  internal static var currentWorkingDirectory: Self { .init(rawValue: SystemLibc.AT_FDCWD) }
+}
+
+// MARK: TTY
+public extension FileDescriptor {
+
+  @inlinable @inline(__always)
+  func isatty() -> Result<Void, Errno> {
+    let ret = SystemLibc.isatty(rawValue)
+    assert([0, 1].contains(ret))
+    if ret == 1 {
+      return .success(())
+    } else {
+      // should be 0
+      return .failure(.systemCurrent)
+    }
+  }
+
+  @inlinable @inline(__always)
+  var isTerminal: Bool {
+    switch isatty() {
+    case .success: return true
+    case .failure: return false
+    }
+  }
+
+  /// name stored in a static buffer which will be overwritten on subsequent calls
+  @inlinable @inline(__always)
+  var unsafeTTYName: StaticCString? {
+    ttyname(rawValue).map { StaticCString(cString: $0) }
+  }
+
+  @inlinable @inline(__always)
+  func getTTYName(into buffer: UnsafeMutablePointer<CChar>, capacity: Int) -> Result<Void, Errno> {
+    SyscallUtilities.errnoOrZeroOnReturn {
+      ttyname_r(rawValue, buffer, capacity)
+    }
+  }
+
+  var ttyName: String? {
+    let capacity = Int(PATH_MAX)
+    return try? String(capacity: capacity) { buffer in
+      switch getTTYName(into: buffer.assumingMemoryBound(to: CChar.self), capacity: capacity) {
+      case .success: break
+      case .failure(let err):
+        assert(err != .outOfRange, "The bufsize is too small!!")
+        throw err
+      }
+      return strlen(buffer)
+    }
+  }
+
 }
