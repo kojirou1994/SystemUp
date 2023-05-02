26e5c40afe6e089f33c25d6c2a36f77257912b02
kojirou <kojirouhtc@gmail.com>
2023-05-02 13:09:58 +0800

    implement flock syscall

--- Sources/SystemUp/FileSyscalls.swift
+++ Sources/SystemUp/FileSyscalls.swift
@@ -266,6 +266,45 @@ public extension FileSyscalls {
 
 }
 
+// MARK: lock
+public extension FileSyscalls {
+
+  struct LockFlags: OptionSet, MacroRawRepresentable {
+
+    public init(rawValue: Int32) {
+      self.rawValue = rawValue
+    }
+
+    public let rawValue: Int32
+
+    /// shared lock
+    @_alwaysEmitIntoClient
+    public static var shared: Self { .init(macroValue: LOCK_SH) }
+
+    /// exclusive lock
+    @_alwaysEmitIntoClient
+    public static var exclusive: Self { .init(macroValue: LOCK_EX) }
+
+    /// don't block when locking
+    @_alwaysEmitIntoClient
+    public static var noBlock: Self { .init(macroValue: LOCK_NB) }
+
+  }
+
+  static func lock(_ fd: FileDescriptor, flags: LockFlags) -> Result<Void, Errno> {
+    SyscallUtilities.voidOrErrno {
+      flock(fd.rawValue, flags.rawValue)
+    }
+  }
+
+  static func unlock(_ fd: FileDescriptor) -> Result<Void, Errno> {
+    SyscallUtilities.voidOrErrno {
+      flock(fd.rawValue, LOCK_UN)
+    }
+  }
+
+}
+
 public extension FileSyscalls {
   struct AtFlags: OptionSet, MacroRawRepresentable {
 
