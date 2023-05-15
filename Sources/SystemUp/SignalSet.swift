eeae79a89050c20f08ae6de87214ec06df120006
kojirou <kojirouhtc@gmail.com>
2023-05-15 13:55:01 +0800

    SignalSet APIs

--- Sources/SystemUp/SignalSet.swift
+++ Sources/SystemUp/SignalSet.swift
@@ -1,4 +1,6 @@
 import SystemLibc
+import CUtility
+import SystemPackage
 
 public struct SignalSet: RawRepresentable {
   public var rawValue: sigset_t
@@ -17,6 +19,7 @@ public struct SignalSet: RawRepresentable {
   }
 }
 
+// MARK: Base APIs
 public extension SignalSet {
   @inlinable @inline(__always)
   @_alwaysEmitIntoClient
@@ -49,4 +52,87 @@ public extension SignalSet {
       sigismember(sigset, signal.rawValue) == 1
     }
   }
+}
+
+// MARK: Syscall APIs
+public extension SignalSet {
+  @inlinable @inline(__always)
+  @_alwaysEmitIntoClient
+  func wait(clearedSignalOutput signal: UnsafeMutablePointer<Signal>) {
+    assertNoFailure { // only fail if set specifies one or more invalid signal numbers.
+      SyscallUtilities.voidOrErrno {
+        withUnsafePointer(to: rawValue) { sigset in
+          sigwait(sigset, .init(OpaquePointer(signal)))
+        }
+      }
+    }
+  }
+
+  @inlinable @inline(__always)
+  @_alwaysEmitIntoClient
+  mutating func getPendingSignals() {
+    assertNoFailure { // The sigpending function does not currently detect any errors.
+      SyscallUtilities.voidOrErrno {
+        sigpending(&rawValue)
+      }
+    }
+  }
+
+  @inlinable @inline(__always)
+  @_alwaysEmitIntoClient
+  @discardableResult
+  mutating func suspend() -> Result<Void, Errno> {
+    SyscallUtilities.voidOrErrno {
+      withUnsafePointer(to: rawValue) { sigset in
+        sigsuspend(sigset)
+      }
+    }
+  }
+
+  @inlinable @inline(__always)
+  @_alwaysEmitIntoClient
+  static var currentMask: Self {
+    var v = Self.init()
+    v.getCurrentMask()
+    return v
+  }
+
+  @inlinable @inline(__always)
+  @_alwaysEmitIntoClient
+  mutating func getCurrentMask() {
+    assertNoFailure {
+      SyscallUtilities.voidOrErrno {
+        sigprocmask(0, nil, &rawValue)
+      }
+    }
+  }
+
+  struct ManipulateMethod: MacroRawRepresentable {
+    public let rawValue: Int32
+    public init(rawValue: Int32) {
+      self.rawValue = rawValue
+    }
+
+    @_alwaysEmitIntoClient
+    public static var add: Self { .init(macroValue: SIG_BLOCK) }
+
+    @_alwaysEmitIntoClient
+    public static var remove: Self { .init(macroValue: SIG_UNBLOCK) }
+
+    @_alwaysEmitIntoClient
+    public static var replace: Self { .init(macroValue: SIG_SETMASK) }
+  }
+
+  @inlinable @inline(__always)
+  @_alwaysEmitIntoClient
+  func manipulateCurrentSignalMask(_ method: ManipulateMethod, oldOutput: UnsafeMutablePointer<Self>?) {
+    assertNoFailure {
+      SyscallUtilities.voidOrErrno {
+        withUnsafePointer(to: rawValue) { sigset in
+          sigprocmask(method.rawValue, sigset, oldOutput?.pointer(to: \.rawValue))
+        }
+      }
+    }
+  }
+
 }
