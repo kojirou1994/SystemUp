c95024208b746795ea623a41c50e153ce3afebf8
kojirou <kojirouhtc@gmail.com>
2024-03-28 10:28:19 +0800

    add ResourceUsage

---
+++ Sources/SystemUp/ResourceUsage.swift
@@ -0,0 +1,40 @@
+import SystemLibc
+import CUtility
+import SystemPackage
+
+public struct ResourceUsage {
+  public var rawValue: rusage
+
+  @_alwaysEmitIntoClient @inlinable @inline(__always)
+  public init() {
+    rawValue = .init()
+  }
+
+  public struct Who: MacroRawRepresentable {
+    public let rawValue: Int32
+    @_alwaysEmitIntoClient @inlinable @inline(__always)
+    public init(rawValue: Int32) {
+      self.rawValue = rawValue
+    }
+  }
+}
+
+public extension ResourceUsage {
+
+  @_alwaysEmitIntoClient @inlinable @inline(__always)
+  mutating func get(for who: Who) {
+    assertNoFailure {
+      SyscallUtilities.voidOrErrno {
+        getrusage(who.rawValue, &rawValue)
+      }
+    }
+  }
+
+}
+
+public extension ResourceUsage.Who {
+  @_alwaysEmitIntoClient
+  static var currentProcess: Self { .init(macroValue: RUSAGE_SELF) }
+  @_alwaysEmitIntoClient
+  static var currentProcessChildren: Self { .init(macroValue: RUSAGE_CHILDREN) }
+}
