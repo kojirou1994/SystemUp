660d27fbea369a90bfc3a2bd1a371399a2c2e451
kojirou <kojirouhtc@gmail.com>
2025-05-04 05:01:17 +0800

    symlink

--- Sources/SystemUp/SystemCall/symlink.swift
+++ Sources/SystemUp/SystemCall/symlink.swift
@@ -1,11 +1,16 @@
 import SystemPackage
 import SystemLibc
+import CUtility
 
 public extension SystemCall {
   @_alwaysEmitIntoClient @inlinable @inline(__always)
-  static func createSymbolicLink(_ linkPath: UnsafePointer<CChar>, relativeTo base: RelativeDirectory = .cwd, toDestination destPath: UnsafePointer<CChar>) -> Result<Void, Errno> {
-    SyscallUtilities.voidOrErrno {
-      symlinkat(destPath, base.toFD, linkPath)
-    }
+  static func createSymbolicLink(_ linkPath: borrowing some CStringConvertible & ~Copyable, relativeTo base: RelativeDirectory = .cwd, toDestination destPath: borrowing some CStringConvertible & ~Copyable) throws(Errno) {
+    try SyscallUtilities.voidOrErrno {
+      linkPath.withUnsafeCString { linkPath in
+        destPath.withUnsafeCString { destPath in
+          symlinkat(destPath, base.toFD, linkPath)
+        }
+      }
+    }.get()
   }
 }
