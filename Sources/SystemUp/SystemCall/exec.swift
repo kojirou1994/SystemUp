6d89111ff29155e0b995759512f3ad73af451f53
kojirou <kojirouhtc@gmail.com>
2025-05-03 21:53:40 +0800

    add exec

---
+++ Sources/SystemUp/SystemCall/exec.swift
@@ -0,0 +1,38 @@
+import SystemPackage
+import SystemLibc
+
+public extension SystemCall {
+
+  @_alwaysEmitIntoClient @inlinable @inline(__always)
+  func exec(_ executablePath: UnsafePointer<CChar>, argv: UnsafePointer<UnsafeMutablePointer<CChar>?>, envp: UnsafePointer<UnsafeMutablePointer<CChar>?>?) throws(Errno) -> Never {
+    try SyscallUtilities.voidOrErrno {
+      // __envp must be non-nil
+      if let envp {
+        return execve(executablePath, argv, envp)
+      } else {
+        // empty environment
+        var termination: UnsafeMutablePointer<CChar>?
+        return execve(executablePath, argv, &termination)
+      }
+    }.get()
+    fatalError()
+  }
+
+  @_alwaysEmitIntoClient @inlinable @inline(__always)
+  func exec(_ executablePath: UnsafePointer<CChar>, argv: UnsafePointer<UnsafeMutablePointer<CChar>?>, searchPATH: Bool) throws -> Never {
+    try SyscallUtilities.voidOrErrno {
+      searchPATH ? execvp(executablePath, argv) : execv(executablePath, argv)
+    }.get()
+    fatalError()
+  }
+
+#if os(macOS) || os(FreeBSD)
+  @_alwaysEmitIntoClient @inlinable @inline(__always)
+  func exec(_ executablePath: UnsafePointer<CChar>, argv: UnsafePointer<UnsafeMutablePointer<CChar>?>, withPATH path: UnsafePointer<CChar>) throws -> Never {
+    // searchpath must be non-nil
+    let code: Int32 = execvP(executablePath, path, argv)
+    fatalError("code should be non -1! \(code) error: \(String(cString: strerror(errno)))")
+  }
+#endif
+
+}
