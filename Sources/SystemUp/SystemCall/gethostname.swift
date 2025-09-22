dedece94664f082c010a12271589f0e0941d660b
kojirou <kojirouhtc@gmail.com>
2025-09-23 03:49:12 +0800

    fix

--- Sources/SystemUp/SystemCall/gethostname.swift
+++ Sources/SystemUp/SystemCall/gethostname.swift
@@ -7,9 +7,9 @@ public extension SystemCall {
 
   @_alwaysEmitIntoClient @inlinable @inline(__always)
   static func gethostname(into buffer: UnsafeMutableBufferPointer<Int8>) throws(Errno) {
-    assert(buffer.count >= sysconf(_SC_HOST_NAME_MAX))
+    assert(buffer.count >= sysconf(numericCast(_SC_HOST_NAME_MAX)))
     try SyscallUtilities.voidOrErrno {
-      SystemLibc.gethostname(buffer.baseAddress, buffer.count)
+      SystemLibc.gethostname(buffer.baseAddress.unsafelyUnwrapped, buffer.count)
     }.get()
   }
 }
