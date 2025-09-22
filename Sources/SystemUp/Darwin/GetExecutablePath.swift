b411a5c19471beb6d4d0b3039a94d078b6c2ee59
kojirou <kojirouhtc@gmail.com>
2025-09-23 03:43:24 +0800

    add getExecutablePath

---
+++ Sources/SystemUp/Darwin/GetExecutablePath.swift
@@ -0,0 +1,22 @@
+#if canImport(Darwin)
+import SystemPackage
+import MachO
+
+public extension SystemCall {
+
+  /// use _NSGetExecutablePath.
+  /// It returns false if the buffer is not large enough, and *size is set to the size required.
+  /// buffer is null-terminated if success.
+  @_alwaysEmitIntoClient @inlinable @inline(__always)
+  static func getExecutablePath(buffer: UnsafeMutablePointer<CChar>?, size: inout UInt32) -> Bool {
+    switch _NSGetExecutablePath(buffer, &size) {
+    case 0: return true
+    case -1: return false
+    default:
+      assertionFailure("Impossible value returned!")
+      return false
+    }
+  }
+
+}
+#endif
