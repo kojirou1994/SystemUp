c288df7c23fa8d35596d4355adfedac827f960db
kojirou <kojirouhtc@gmail.com>
2025-09-25 07:41:40 +0800

    update chmod

--- Sources/SystemUp/SystemCall/chmod.swift
+++ Sources/SystemUp/SystemCall/chmod.swift
@@ -1,20 +1,23 @@
 import SystemPackage
+import CUtility
 import SystemLibc
 
 public extension SystemCall {
 
   @_alwaysEmitIntoClient @inlinable @inline(__always)
-  static func set(permissions: FilePermissions, for path: UnsafePointer<CChar>, relativeTo base: RelativeDirectory = .cwd, flags: AtFlags = []) -> Result<Void, Errno> {
+  static func set(permissions: FilePermissions, for path: borrowing some CString, relativeTo base: RelativeDirectory = .cwd, flags: AtFlags = []) throws(Errno) {
     assert(flags.isSubset(of: [.noFollow]))
-    return SyscallUtilities.voidOrErrno {
-      fchmodat(base.toFD, path, permissions.rawValue, flags.rawValue)
-    }
+    try SyscallUtilities.voidOrErrno {
+      path.withUnsafeCString { path in
+        SystemLibc.fchmodat(base.toFD, path, permissions.rawValue, flags.rawValue)
+      }
+    }.get()
   }
 
   @_alwaysEmitIntoClient @inlinable @inline(__always)
-  static func set(permissions: FilePermissions, for fd: FileDescriptor) -> Result<Void, Errno> {
-    SyscallUtilities.voidOrErrno {
-      fchmod(fd.rawValue, permissions.rawValue)
-    }
+  static func set(permissions: FilePermissions, for fd: FileDescriptor) throws(Errno) {
+    try SyscallUtilities.voidOrErrno {
+      SystemLibc.fchmod(fd.rawValue, permissions.rawValue)
+    }.get()
   }
 }
