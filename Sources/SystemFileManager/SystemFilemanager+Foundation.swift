4f825b4d27f06565f4c7e667a973d00b9e035bc9
kojirou <kojirouhtc@gmail.com>
2025-11-30 04:47:30 +0800

    update

--- Sources/SystemFileManager/SystemFilemanager+Foundation.swift
+++ Sources/SystemFileManager/SystemFilemanager+Foundation.swift
@@ -1,16 +1,21 @@
+#if !$Embedded
 import Foundation
+import CUtility
 
 extension SystemFileManager {
   @available(iOS 11.0, *)
-  public static func delete(_ path: FilePath, trashIfAvailable: Bool, or removeMethod: (FilePath) throws -> Void = { try remove($0) }) throws {
-    #if os(macOS) || os(iOS)
-    if trashIfAvailable {
-      try FileManager.default.trashItem(at: URL(fileURLWithPath: path.string), resultingItemURL: nil)
-    } else {
+  public static func delete(_ path: borrowing some CString, trashIfAvailable: Bool, or removeMethod: (UnsafePointer<CChar>) throws -> Void = { try SystemFileManager.remove($0) }) throws {
+    try path.withUnsafeCString { path in
+      #if os(macOS) || os(iOS)
+      if trashIfAvailable {
+        try FileManager.default.trashItem(at: URL(fileURLWithFileSystemRepresentation: path, isDirectory: false, relativeTo: nil), resultingItemURL: nil)
+      } else {
+        try removeMethod(path)
+      }
+      #else
       try removeMethod(path)
+      #endif
     }
-    #else
-    try removeMethod(path)
-    #endif
   }
 }
+#endif
