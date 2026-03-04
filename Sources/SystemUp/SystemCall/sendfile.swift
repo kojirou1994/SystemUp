d9a168562fdef9afdd2a1f03433fa5293781dea1
kojirou <kojirouhtc@gmail.com>
2026-03-05 00:07:29 +0800

    add sendfile

---
+++ Sources/SystemUp/SystemCall/sendfile.swift
@@ -0,0 +1,23 @@
+import SystemLibc
+
+public extension SystemCall {
+
+  #if APPLE
+  @_alwaysEmitIntoClient @inlinable @inline(__always)
+  static func sendfile(from src: FileDescriptor, to dst: FileDescriptor, offset: Int64, bytes: UnsafeMutablePointer<Int64>, hdtr: UnsafeMutableRawPointer? = nil) throws(Errno) {
+    // The flags parameter is reserved for future expansion and must be set to 0. Any other value will cause sendfile() to return EINVAL.
+    try SyscallUtilities.voidOrErrno {
+      SystemLibc.sendfile(src.rawValue, dst.rawValue, offset, bytes, hdtr?.assumingMemoryBound(to: sf_hdtr.self), 0)
+    }.get()
+  }
+  #endif
+
+  #if os(Linux)
+  @_alwaysEmitIntoClient @inlinable @inline(__always)
+  static func sendfile(from src: FileDescriptor, to dst: FileDescriptor, offset: UnsafeMutablePointer<Int>?, count: Int) throws(Errno) {
+    try SyscallUtilities.voidOrErrno {
+      SystemLibc.sendfile(dst.rawValue, src.rawValue, offset, count)
+    }.get()
+  }
+  #endif
+}
